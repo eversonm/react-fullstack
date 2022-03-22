96d56329cea356a9c260c57e23e399a4f7cd5915
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-03-22 15:43:45 -0300

    Adding a UsersList Page / Component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './UserItem.css';
+
+const UserItem = props => {
+	return <h2>Bem vindo</h2>
+}
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,14 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-	return <h2>Bem vindo</h2>
+	const USERS = [{
+		id: 'u1', 
+		name: "everson", 
+		image: "https://i.pinimg.com/originals/e1/55/07/e155078c2fd1890340745b1fcd697a0d.jpg", 
+		places: 3
+	}];
+	return <UsersList items={USERS} />
 }
 
 export default Users;
