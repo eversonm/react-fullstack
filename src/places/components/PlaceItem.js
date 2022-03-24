f31af5c1953412eaa50b0b82302df838e33ca6c2
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-03-24 16:43:18 -0300

    Google Maps API

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
@@ -25,7 +26,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>Close</Button>}
       >
         <div className="map-container">
-          <h2>The map</h2>
+          <Map center={props.coordinates} zoom={16}/>
         </div>
       </Modal>
       <li className="place-item">
