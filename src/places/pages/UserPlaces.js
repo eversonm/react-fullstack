746a04bd7ec15afed89482a1f0e44321f290ca1a
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-03-24 15:38:15 -0300

    Creating a Modal and ModalOverlay

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList';
+import PlaceList from "../components/PlaceList";
 
-const DUMMY_PLACES = [{
-  id: 'p1',
-  title: 'Empire State Building',
-  description: 'One of the most famous sky scrapers in the world!',
-  imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg',
-  address: '20 W 34th St, New York, NY 10001, Estados Unidos',
-  location: {
-    lat: 40.7484405,
-    lng: -73.9878584
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
   },
-  creator: 'u1'
-},
-{
-  id: 'p2',
-  title: 'Empire State Building',
-  description: 'One of the most famous sky scrapers in the world!',
-  imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg',
-  address: '20 W 34th St, New York, NY 10001, Estados Unidos',
-  location: {
-    lat: 40.7484405,
-    lng: -73.9878584
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
   },
-  creator: 'u2'
-}
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />
-}
+  const userId = useParams().userId;
+  const loaderPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+  return <PlaceList items={loaderPlaces} />;
+};
 
 export default UserPlaces;
