d8102dd8192b1a7d39109fdcdca77bde1b7ab52f
Everson Magalhães <everson.m.cavalcante@gmail.com>
2022-03-31 21:58:50 -0300

    Working with custom hooks and Update Place

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, Estados Unidos",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const idPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: idPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: idPlace.description,
+          isValid: true,
+        },
+      },
+      true
+    );
+    setIsLoading(false)
+  }, [setFormData, idPlace]);
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  if (!idPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 characteres)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        Update Place
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
